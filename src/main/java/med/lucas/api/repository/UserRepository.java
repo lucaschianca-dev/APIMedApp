ee9e127193d4714a0fd78ffcfdc17cbaf518cbaf
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-13 16:21:48 -0300

    UserRepository updated. Created services package. Created AutenticacaoService

--- src/main/java/med/lucas/api/repository/UserRepository.java
+++ src/main/java/med/lucas/api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package med.lucas.api.repository;
 
 import med.lucas.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login); //Método que vai fazer a consulta de usuários no banco de dados
 }
---
+++ src/main/java/med/lucas/api/services/AutenticacaoService.java
@@ -0,0 +1,23 @@
+package med.lucas.api.services;
+
+import med.lucas.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//Serviço de autenticação
+//Para o Spring Security saiba que essa classe representa o serviço de autenticação
+//Implementamos a Interface UserDetailsService
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
